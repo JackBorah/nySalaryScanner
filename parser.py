be480308ef31336647382354fabc2a666955ff33
Jack Borah <71891256+JackBorah@users.noreply.github.com>
2026-04-01 14:09:21 -0400

    first commit

---
+++ parser.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup
+from dataclasses import asdict, dataclass
+import csv
+
+@dataclass
+class Employee:
+    name: str
+    employer: str
+    total_pay: str
+    subagency: str
+    title: str
+    rate_of_pay: str
+    pay_year: str
+    pay_basis: str
+    major_category: str
+
+employees = []
+
+with open("westbury_results.html") as f:
+    soup = BeautifulSoup(f, 'html.parser')
+    data = soup.find_all('tr')[1:]
+    for i in range(0, len(data), 2):
+        cells = data[i].find_all("td")
+        additional_cells = data[i+1].find_all(class_="col-xs-6")
+        employees.append(Employee(
+            cells[1].get_text(),
+            cells[2].get_text(),
+            cells[3].get_text(),
+            cells[4].get_text(),
+            additional_cells[1].get_text(),
+            additional_cells[2].get_text(),
+            additional_cells[3].get_text(),
+            additional_cells[4].get_text(),
+            additional_cells[5].get_text(),
+        ))
+
+with open("westbury_salaries.csv", "w", newline="") as f:
+    writer = csv.DictWriter(
+        f,
+        fieldnames=Employee.__dataclass_fields__.keys()
+    )
+
+    writer.writeheader()
+
+    for emp in employees:
+        writer.writerow(asdict(emp))
